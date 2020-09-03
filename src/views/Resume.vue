d42eb5668e01bedf841fb0f62c20a3840665f23a
ReformedCola <reformedcola@gmail.com>
2020-09-03 12:42:56 -0700

    Add Resume page

---
+++ src/views/Resume.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href='src/assets/JasonHe_Resume.pdf' target="_blank">Resume</a>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component} from 'vue-property-decorator'
+
+  @Component
+  export default class Resume extends Vue {
+
+  }
+</script>
+
+<style>
+
+</style>
